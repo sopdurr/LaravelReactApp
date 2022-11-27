967d05d19e04e0393495ae73e1baf57a129b4c22
Sigurdur Jonsson <siggijons29@gmail.com>
2022-11-27 12:01:41 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import BookList from "./components/list.component";
+import CreateNew from "./components/createNew";
+import HomeScreen from "./components/HomeScreen";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Router>
+      <div>
+        <div className="header">
+          <h3>Fantastik4 BlogPage</h3>
+        </div>
+        <div className="navbar d-flex justify-content-center">
+          <div className="router">
+            <nav class="navbar navbar-expand-lg">
+              <div id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link">
+                      <Link className="theLink" to="/">
+                        Home
+                      </Link>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link">
+                      <Link className="theLink" to="/booklist">
+                        Blog List
+                      </Link>
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link">
+                      <Link className="theLink" to="/create">
+                        Create Blog
+                      </Link>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+        <div className="background-image">
+          <Routes>
+          <Route path="/" element={<HomeScreen />} />
+            <Route path="/booklist" element={<BookList />} />
+            <Route path="/create" element={<CreateNew />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
     </div>
   );
 }
